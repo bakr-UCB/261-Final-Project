05a42ad917da1ac82b7f3367701fe6551ea4e239
erica-landreth <erica-landreth>
2025-03-29 01:14:58 +0000

    Progress as of 3/28 (Phase II)

--- flightdelays/dataset.py
+++ flightdelays/dataset.py
@@ -1,6 +1,7 @@
 def testfunc(param):
     # Function implementation here
+
     pass
 
-
-testfunc(1)
+if __name__ == "__main__":
+    testfunc(1)
---
+++ flightdelays/utils.py
@@ -0,0 +1,18 @@
+from databricks.sdk.runtime import dbutils
+
+def lst_files_r(path):
+    files = []
+    dirs = [path]
+    
+    while dirs:
+        current_dir = dirs.pop()
+        for item in dbutils.fs.ls(current_dir):
+            if item.isDir():
+                dirs.append(item.path)
+            else:
+                files.append(item.path)
+    
+    return files
+
+if __name__ == '__main__':
+    print(lst_files_r('dbfs:/student-groups/Group_4_1'))
